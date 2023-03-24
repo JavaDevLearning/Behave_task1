a99e47727cbb493b03886ded20b2f819ab921886
Andrey Belkin <dkvd1@yandex.ru>
2023-03-24 18:45:03 +0500

    fixed

--- src/Randoms.java
+++ src/Randoms.java
@@ -2,24 +2,30 @@ import java.util.Iterator;
 import java.util.PrimitiveIterator;
 import java.util.Random;
 
+import java.util.Iterator;
+import java.util.Random;
+
 public class Randoms implements Iterable<Integer> {
-    protected PrimitiveIterator.OfInt random;
+    protected Random random = new Random();
+    int min;
+    int max;
 
     public Randoms(int min, int max) {
-        random = new Random().ints(min, max + 1).iterator();
+        this.min = min;
+        this.max = max;
     }
 
     @Override
     public Iterator<Integer> iterator() {
-        return new Iterator<Integer>() {
+        return new Iterator<>() {
             @Override
             public boolean hasNext() {
-                return true;//всегда true для бесконечности
+                return true;
             }
 
             @Override
             public Integer next() {
-                return random.nextInt();//возвращаем следующее сгенерированное число
+                return random.ints(min, max + 1).findFirst().getAsInt();
             }
         };
     }
